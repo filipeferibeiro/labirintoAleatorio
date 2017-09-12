dcd1c0a2aeb4b666c626a1033687710b0d6f9212
Guilherme Pereira <guilherme27.gp@gmail.com>
2017-09-12 12:19:28 -0300

    Adicionando arquivos

--- LabirintoAleatorio/src/labirintoaleatorio/Labirinto.java
+++ LabirintoAleatorio/src/labirintoaleatorio/Labirinto.java
@@ -5,21 +5,66 @@ import java.util.Random;
 import java.lang.Math;
 
 public class Labirinto {
-    private int [][] matriz;
+    public class Sala {
+        int qntSala;
+        boolean trap, key, exit;
+        
+        public Sala(int qntSala, boolean trap, boolean key, boolean exit){
+            this.qntSala = qntSala;
+            this.trap = trap;
+            this.key = key;
+            this.exit = exit;
+        }
+
+        public int getQntSala() {
+            return qntSala;
+        }
+
+        public boolean isTrap() {
+            return trap;
+        }
+
+        public boolean isKey() {
+            return key;
+        }
+
+        public boolean isExit() {
+            return exit;
+        }
+
+        public void setTrap(boolean trap) {
+            this.trap = trap;
+        }
+
+        public void setKey(boolean key) {
+            this.key = key;
+        }
+        
+    }
+    
+    
+    private Sala [][] matriz;
     
     public Labirinto () {
         Random rand = new Random();
-        int tamanho = 9 + rand.nextInt(3);
-        tamanho = (int) Math.pow(tamanho, 2);
-        this.matriz = new int [tamanho][tamanho];
+        int tamanho = 32 + rand.nextInt(14);
+        int tamanho2 = (int) Math.pow(tamanho, 2);
+        this.matriz = new Sala [tamanho2][tamanho2];
         
         for (int i = 0; i < tamanho; i++) {
             for (int j = 0; j < tamanho; j++){
                 int cont = 0;
+                
             }
         }
         
+        System.out.println(tamanho);
     }
-        
+    
+    
+    
+    public static void main(String[] args) {
+        Labirinto lab = new Labirinto();
+    }
     
 }
