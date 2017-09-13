401f9c85394d5a0913b35b465d5cca02a83b11be
Filipe Fernandes <filipe1.2012@gmail.com>
2017-09-13 17:37:43 -0300

    Atualizando.

--- LabirintoAleatorio/src/labirintoaleatorio/Home.java
+++ LabirintoAleatorio/src/labirintoaleatorio/Home.java
@@ -5,6 +5,8 @@
  */
 package labirintoaleatorio;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author filipeferibeiro
@@ -29,11 +31,15 @@ public class Home extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        lblNome = new javax.swing.JLabel();
         txtPlayerName = new javax.swing.JTextField();
         jSeparator1 = new javax.swing.JSeparator();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        lblDificuldade = new javax.swing.JLabel();
+        selectDificuldade = new javax.swing.JComboBox<>();
+        btJogar = new javax.swing.JPanel();
+        lblIMGJogar = new javax.swing.JLabel();
+        btAjuda = new javax.swing.JPanel();
+        lblIMGAjuda = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(java.awt.Color.white);
@@ -45,7 +51,7 @@ public class Home extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 250, Short.MAX_VALUE)
+            .addGap(0, 201, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -54,57 +60,112 @@ public class Home extends javax.swing.JFrame {
 
         jPanel2.setBackground(java.awt.Color.white);
 
-        jLabel1.setFont(new java.awt.Font("Ubuntu", 1, 11)); // NOI18N
-        jLabel1.setText("NOME");
+        lblNome.setFont(new java.awt.Font("Ubuntu", 1, 11)); // NOI18N
+        lblNome.setText("NOME");
 
-        txtPlayerName.setBackground(java.awt.Color.white);
         txtPlayerName.setBorder(null);
 
         jSeparator1.setBackground(java.awt.Color.black);
 
-        jButton1.setText("Ajuda");
+        lblDificuldade.setFont(new java.awt.Font("Ubuntu", 1, 11)); // NOI18N
+        lblDificuldade.setText("DIFICULDADE");
 
-        jButton2.setText("JOGAR!");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+        selectDificuldade.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione", "Fácil", "Médio", "Difícil" }));
+
+        btJogar.setBackground(new java.awt.Color(255, 255, 255));
+        btJogar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btJogarMouseClicked(evt);
             }
         });
+
+        lblIMGJogar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/labirintoaleatorio/images/btJogarN.png"))); // NOI18N
+
+        javax.swing.GroupLayout btJogarLayout = new javax.swing.GroupLayout(btJogar);
+        btJogar.setLayout(btJogarLayout);
+        btJogarLayout.setHorizontalGroup(
+            btJogarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, btJogarLayout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addComponent(lblIMGJogar)
+                .addGap(0, 0, 0))
+        );
+        btJogarLayout.setVerticalGroup(
+            btJogarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, btJogarLayout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addComponent(lblIMGJogar)
+                .addGap(0, 0, 0))
+        );
+
+        btAjuda.setBackground(new java.awt.Color(255, 255, 255));
+
+        lblIMGAjuda.setIcon(new javax.swing.ImageIcon(getClass().getResource("/labirintoaleatorio/images/btAjudaN.png"))); // NOI18N
+
+        javax.swing.GroupLayout btAjudaLayout = new javax.swing.GroupLayout(btAjuda);
+        btAjuda.setLayout(btAjudaLayout);
+        btAjudaLayout.setHorizontalGroup(
+            btAjudaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(btAjudaLayout.createSequentialGroup()
+                .addComponent(lblIMGAjuda)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        btAjudaLayout.setVerticalGroup(
+            btAjudaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(btAjudaLayout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addComponent(lblIMGAjuda)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(txtPlayerName)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(jSeparator1))
+                        .addContainerGap()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtPlayerName)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(lblNome)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addComponent(jSeparator1)))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblDificuldade)
+                            .addComponent(selectDificuldade, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(100, Short.MAX_VALUE)
+                .addComponent(btJogar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(100, 100, 100))
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(43, 43, 43)
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 163, Short.MAX_VALUE)
-                .addComponent(jButton2)
-                .addGap(43, 43, 43))
+                .addGap(119, 119, 119)
+                .addComponent(btAjuda, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addComponent(lblNome)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtPlayerName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(3, 3, 3)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 182, Short.MAX_VALUE)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addGap(91, 91, 91))
+                .addGap(5, 5, 5)
+                .addComponent(lblDificuldade)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(selectDificuldade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(80, 80, 80)
+                .addComponent(btJogar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btAjuda, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -125,10 +186,20 @@ public class Home extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        this.setVisible(false);
-        new Game(txtPlayerName.getText()).setVisible(true);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void btJogarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btJogarMouseClicked
+        if (txtPlayerName.getText().equals("")){
+            JOptionPane.showMessageDialog(null, "Por favor informe seu nome jogador!");
+        }
+        else {
+            if (selectDificuldade.getSelectedIndex() == 0) {
+            JOptionPane.showMessageDialog(null, "Por favor selecione uma dificuldade!");
+            }
+            else {
+                this.setVisible(false);
+                new Game(txtPlayerName.getText()).setVisible(true);
+            }
+        }
+    }//GEN-LAST:event_btJogarMouseClicked
 
     /**
      * @param args the command line arguments
@@ -166,12 +237,16 @@ public class Home extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel btAjuda;
+    private javax.swing.JPanel btJogar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel lblDificuldade;
+    private javax.swing.JLabel lblIMGAjuda;
+    private javax.swing.JLabel lblIMGJogar;
+    private javax.swing.JLabel lblNome;
+    private javax.swing.JComboBox<String> selectDificuldade;
     private javax.swing.JTextField txtPlayerName;
     // End of variables declaration//GEN-END:variables
 }
