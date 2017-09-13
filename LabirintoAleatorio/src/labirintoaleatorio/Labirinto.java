cef7dee8dd3132ac410bc895b73ffe309ed84e12
Guilherme Pereira <guilherme27.gp@gmail.com>
2017-09-13 17:44:44 -0300

    adicionando algoritmo geração do labirinto em grade

--- LabirintoAleatorio/src/labirintoaleatorio/Labirinto.java
+++ LabirintoAleatorio/src/labirintoaleatorio/Labirinto.java
@@ -47,18 +47,72 @@ public class Labirinto {
     
     public Labirinto () {
         Random rand = new Random();
-        int tamanho = 32 + rand.nextInt(14);
-        int tamanho2 = (int) Math.pow(tamanho, 2);
+        int tamanho = rand.nextInt(3);
+        int tamanho2;
+        
+        switch(tamanho){
+            case 0:
+                tamanho = 16;
+                tamanho2 = 160; // 10 x 16
+                break;
+            case 1:
+                tamanho = 18;
+                tamanho2 = 216; // 12 x 18
+                break;
+            case 2:
+                tamanho = 20;
+                tamanho2 = 280; // 14 x 20
+                break;
+            default:
+                tamanho = 20;
+                tamanho2 = 280;
+                break;
+        }
+        
+        
         this.matriz = new Sala [tamanho2][tamanho2];
         
-        for (int i = 0; i < tamanho; i++) {
-            for (int j = 0; j < tamanho; j++){
-                int cont = 0;
+        int cont = 1;
+        int coluna = 1;
+        boolean flag = false;
+        for (int i = 0; i < tamanho2; i++) {
+            System.out.println("\n");
+            flag = true;
+            for (int j = 0; j < tamanho2; j++){
+                if (flag){
+                    if (cont < (tamanho - 1) && (coluna < tamanho2)){
+                        this.matriz[i][coluna] = new Sala(1, false, false, false);
+                        cont++;
+                    }
+                    else{
+                        cont = 1;
+                    }
+                    if (coluna + (tamanho - 1) < tamanho2){
+                        this.matriz[i][coluna + (tamanho - 1)] = new Sala(1, false, false, false);
+                    }
+                    flag = false;
+                    coluna++;
+                }
+                /*else if(j != cont && this.matriz[i][j] == null){
+                    //this.matriz[i][j] = new Sala(0, false, false, false);
+                }*/
+                if (this.matriz[i][j] != null){
+                    System.out.print(this.matriz[i][j].qntSala);
+                }
+                else{
+                    System.out.print("0");
+                }
+                
+                
+                /*if (cont == tamanho){
+                    matriz[i][j]
+                    
+                }*/
                 
             }
         }
         
-        System.out.println(tamanho);
+        System.out.println("\n\n\n" + tamanho + " tamanho²: " + tamanho2);
     }
     
     
