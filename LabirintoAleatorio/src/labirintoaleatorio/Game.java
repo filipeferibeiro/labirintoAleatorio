7f6f03e9634dfeaab2558a81ab5c7cc632ed3ec0
Filipe Fernandes <filipe1.2012@gmail.com>
2017-09-17 18:39:51 -0300

    Agora gerando as labels que serão as salas de forma dinâmica de acordo com a dificuldade escolhida.

--- LabirintoAleatorio/src/labirintoaleatorio/Game.java
+++ LabirintoAleatorio/src/labirintoaleatorio/Game.java
@@ -1,11 +1,42 @@
 package labirintoaleatorio;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
 public class Game extends javax.swing.JFrame {
 
-    public Game(String playerName) {
+    public Game(String playerName, int tam) {
         initComponents();
         player = new Player(playerName);
         lblPlayerName.setText(player.getPlayerName());
+        setSize(875, 530);
+        
+        if (tam == 160) {
+            gamePanel.setLayout(new java.awt.GridLayout(0, 16));
+        }
+        else if (tam == 216) {
+            gamePanel.setLayout(new java.awt.GridLayout(0, 18));
+        }
+        else {
+            gamePanel.setLayout(new java.awt.GridLayout(0, 20));
+        }
+        
+        createMatriz(tam);
+    }
+    
+    public void createMatriz(int tam) {
+        this.matriz = new JLabel[tam];        
+        for (int i = 0; i < tam; i++) {
+            matriz[i] = new JLabel("" + i, SwingConstants.CENTER);
+            gamePanel.add(matriz[i]);
+            
+        }
+        gamePanel.updateUI();
     }
 
     @SuppressWarnings("unchecked")
@@ -19,8 +50,10 @@ public class Game extends javax.swing.JFrame {
         life1 = new javax.swing.JLabel();
         life2 = new javax.swing.JLabel();
         life3 = new javax.swing.JLabel();
+        teste = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        gamePanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -42,6 +75,13 @@ public class Game extends javax.swing.JFrame {
 
         life3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/labirintoaleatorio/images/icons8_Hearts_25px_3.png"))); // NOI18N
 
+        teste.setText("jButton1");
+        teste.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                testeActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -60,7 +100,9 @@ public class Game extends javax.swing.JFrame {
                         .addComponent(life2)
                         .addGap(0, 0, 0)
                         .addComponent(life3)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 545, Short.MAX_VALUE)
+                .addComponent(teste)
+                .addGap(51, 51, 51)
                 .addComponent(lblKeys)
                 .addGap(18, 18, 18))
         );
@@ -81,34 +123,36 @@ public class Game extends javax.swing.JFrame {
                             .addComponent(life3)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(17, 17, 17)
-                        .addComponent(lblKeys)))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblKeys)
+                            .addComponent(teste))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jPanel2.setBackground(java.awt.Color.white);
 
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+        jScrollPane1.setBackground(new java.awt.Color(255, 255, 255));
+        jScrollPane1.setBorder(null);
+
+        gamePanel.setBackground(new java.awt.Color(255, 255, 255));
+        gamePanel.setLayout(new java.awt.GridLayout(0, 16));
+        jScrollPane1.setViewportView(gamePanel);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(314, 314, 314)
-                .addComponent(jButton1)
-                .addContainerGap(396, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jScrollPane1)
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(72, 72, 72)
-                .addComponent(jButton1)
-                .addContainerGap(365, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 441, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -129,10 +173,15 @@ public class Game extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void testeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_testeActionPerformed
         player.addKeys();
         lblKeys.setText(player.getKeys() + " / 3");
-    }//GEN-LAST:event_jButton1ActionPerformed
+        //createMatriz();
+        //JLabel label = new JLabel("Ha");
+        //gamePanel.add(label);
+        //matriz.add(label);
+        //gamePanel.updateUI();
+    }//GEN-LAST:event_testeActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -167,16 +216,19 @@ public class Game extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel gamePanel;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblKeys;
     private javax.swing.JLabel lblPlayerImg;
     private javax.swing.JLabel lblPlayerName;
     private javax.swing.JLabel life1;
     private javax.swing.JLabel life2;
     private javax.swing.JLabel life3;
+    private javax.swing.JButton teste;
     // End of variables declaration//GEN-END:variables
     private Labirinto lab;
     private Player player;
+    private JLabel matriz[];
 }
